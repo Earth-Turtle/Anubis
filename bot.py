14388b52f3bd6619d1884ab64a06f8c174adb7eb
EarthTurtle <earthenturtle@gmail.com>
2023-04-08 13:34:30 -0700

    Fixed issues, typing indicator still not showing up

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 # bot.py
-import asyncio
 from collections import defaultdict
 import os
+from typing import Union
 
 from discord import *
+from discord.utils import get
 from dotenv import load_dotenv
 from discord.ext.commands import Bot, Context
 
@@ -21,41 +22,55 @@ async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
 @bot.command(name='reactions_given', help='Generates stats for a specific user on their reactions given')
-async def reactions_given(ctx: Context, user: User, amount: int = 200):
+async def reactions_given(ctx: Context, user: User, amount: int = 200, channel: TextChannel = None):
     stats = defaultdict(int)
-    messages = asyncio.run(get_history(ctx.channel, amount))
-    for message in messages:
+    channel = channel if channel else ctx.channel
+    await channel.typing()
+    messages = await get_history(channel, amount)
+    async for message in messages:
         reactions = message.reactions
         for reaction in reactions:
             async for reactor in reaction.users():
                 if reactor.name == user.name:
                     stats[str(reaction.emoji)] += 1
-    output = '\n'.join([f'{k}: {v}' for k, v in sorted(stats.items(), key=lambda item: item[1], reverse=True)])
-    await ctx.send("Stats for " + user.mention)
-    await ctx.send(output)
+    await ctx.send("Reactions given by " + user.mention)
+    await ctx.send(format_stats(stats))
             
     
 
-@bot.command(name='reactions_received', help='Genetates stats for a specific user on their reactions received')
-async def reactions_received(ctx: Context, user: User, amount: int = 200):
-    pass
+@bot.command(name='reactions_received', help='Genetates stats for a specific user on their reactions received. Mention the user as first argument, number of messages to check as the second')
+async def reactions_received(ctx: Context, user: User, amount: int = 200, channel: TextChannel = None):
+    stats = defaultdict(int)
+    channel = channel if channel else ctx.channel
+    await channel.typing()
+    messages = await get_history(channel, amount)
+    count = 0
+    async for message in messages:
+        if message.author.name == user.name:
+            count += 1
+            reactions = message.reactions
+            for reaction in reactions:
+                stats[str(reaction.emoji)] += reaction.count
+    await ctx.send(f"Reactions {user.mention} has received, from {count} posts:")
+    await ctx.send(format_stats(stats))
 
 @bot.event
 async def on_command_error(ctx, error):
     await ctx.send('Something went wrong, check stdout for details')
     raise error
 
-async def get_history(channel: abc.Messageable, amount: int) -> list[Message]:
+async def get_history(channel: abc.Messageable, amount: int):
     try:
-        async with channel.typing():
-            await asyncio.sleep(5)
-        return [m async for m in channel.history(limit=amount)]
+        return channel.history(limit=amount)
     except Forbidden:
         print("Bot does not have history permissions")
         await channel.send("Bot does not have history permissions")
         return None
 
-        
+def format_stats(stats: dict):
+    if not stats:
+        return "No information found for user"
+    return '\n'.join([f'{k}: {v}' for k, v in sorted(stats.items(), key=lambda item: item[1], reverse=True)])
 
 
 bot.run(TOKEN)
