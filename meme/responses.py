d15b07314940bd71f7198e6b93c04c540e6f52d6
EarthTurtle <earthenturtle@gmail.com>
2023-08-05 02:28:18 -0700

    expanded response regex

--- meme/responses.py
+++ meme/responses.py
@@ -3,7 +3,7 @@ import re
 
 class Responses:
     def find_response(self, message: Message) -> str:
-        if re.search("she [A-Za-z]+ on my [A-Za-z]+ till?'? (i|im|i'm|my) [A-Za-z]+", message.content.lower()):
+        if re.search("she [A-Za-z0-9 ]+ on my [A-Za-z0-9 ]+ till?'? (i|im|i'm|my) [A-Za-z0-9 ]+", message.content.lower()):
             return "`EXTREMELY LOUD INCORRECT BUZZER`"
         if message.author.name.lower() == "biological_jessie" and re.search(":3", message.content):
             return "https://cdn.discordapp.com/attachments/525173455516336129/1135766326233796608/rapidsave.com_meow-fujthvqougeb1.mp4"
